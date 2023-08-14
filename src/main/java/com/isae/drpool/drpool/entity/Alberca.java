36077cceee9c8fd6a5d0f07ce8971f41d9424c6d
Jorge <jorgeluis.gomez.sat@gmail.com>
2023-08-14 12:25:37 -0600

    se agregaron los estatus de sedes y albercas

--- src/main/java/com/isae/drpool/drpool/entity/Alberca.java
+++ src/main/java/com/isae/drpool/drpool/entity/Alberca.java
@@ -29,6 +29,7 @@ public class Alberca {
 	@OneToOne
 	@JoinColumn(name="idsede")
 	private Sede sede;
+	private String estatus;
 	private String equiponombre;
 	private String tipoequipo;
 	
@@ -36,9 +37,11 @@ public class Alberca {
 	public Alberca() {
 		super();
 	}
+
+
 	public Alberca(int idalberca, String nombrealberca, String tipoalberca, String capacidad, String medidalargo,
-			String medidaancho, String medidaprofundidad, String ubicacion, Sede sede, String equiponombre,
-			String tipoequipo) {
+			String medidaancho, String medidaprofundidad, String ubicacion, Sede sede, String estatus,
+			String equiponombre, String tipoequipo) {
 		super();
 		this.idalberca = idalberca;
 		this.nombrealberca = nombrealberca;
@@ -49,74 +52,139 @@ public class Alberca {
 		this.medidaprofundidad = medidaprofundidad;
 		this.ubicacion = ubicacion;
 		this.sede = sede;
+		this.estatus = estatus;
 		this.equiponombre = equiponombre;
 		this.tipoequipo = tipoequipo;
 	}
+
+
 	public int getIdalberca() {
 		return idalberca;
 	}
+
+
 	public void setIdalberca(int idalberca) {
 		this.idalberca = idalberca;
 	}
+
+
 	public String getNombrealberca() {
 		return nombrealberca;
 	}
+
+
 	public void setNombrealberca(String nombrealberca) {
 		this.nombrealberca = nombrealberca;
 	}
+
+
 	public String getTipoalberca() {
 		return tipoalberca;
 	}
+
+
 	public void setTipoalberca(String tipoalberca) {
 		this.tipoalberca = tipoalberca;
 	}
+
+
 	public String getCapacidad() {
 		return capacidad;
 	}
+
+
 	public void setCapacidad(String capacidad) {
 		this.capacidad = capacidad;
 	}
+
+
 	public String getMedidalargo() {
 		return medidalargo;
 	}
+
+
 	public void setMedidalargo(String medidalargo) {
 		this.medidalargo = medidalargo;
 	}
+
+
 	public String getMedidaancho() {
 		return medidaancho;
 	}
+
+
 	public void setMedidaancho(String medidaancho) {
 		this.medidaancho = medidaancho;
 	}
+
+
 	public String getMedidaprofundidad() {
 		return medidaprofundidad;
 	}
+
+
 	public void setMedidaprofundidad(String medidaprofundidad) {
 		this.medidaprofundidad = medidaprofundidad;
 	}
+
+
 	public String getUbicacion() {
 		return ubicacion;
 	}
+
+
 	public void setUbicacion(String ubicacion) {
 		this.ubicacion = ubicacion;
 	}
+
+
 	public Sede getSede() {
 		return sede;
 	}
+
+
 	public void setSede(Sede sede) {
 		this.sede = sede;
 	}
+
+
+	public String getEstatus() {
+		return estatus;
+	}
+
+
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+
+
 	public String getEquiponombre() {
 		return equiponombre;
 	}
+
+
 	public void setEquiponombre(String equiponombre) {
 		this.equiponombre = equiponombre;
 	}
+
+
 	public String getTipoequipo() {
 		return tipoequipo;
 	}
+
+
 	public void setTipoequipo(String tipoequipo) {
 		this.tipoequipo = tipoequipo;
 	}
+
+
+	@Override
+	public String toString() {
+		return "Alberca [idalberca=" + idalberca + ", nombrealberca=" + nombrealberca + ", tipoalberca=" + tipoalberca
+				+ ", capacidad=" + capacidad + ", medidalargo=" + medidalargo + ", medidaancho=" + medidaancho
+				+ ", medidaprofundidad=" + medidaprofundidad + ", ubicacion=" + ubicacion + ", sede=" + sede
+				+ ", estatus=" + estatus + ", equiponombre=" + equiponombre + ", tipoequipo=" + tipoequipo + "]";
+	}
+	
 	
 }
--- src/main/java/com/isae/drpool/drpool/entity/Sede.java
+++ src/main/java/com/isae/drpool/drpool/entity/Sede.java
@@ -4,7 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -21,12 +23,17 @@ public class Sede {
 	private String encargado;
 	private String telefono;
 	private String correo;
+	private String estatus;
+	@OneToOne
+	@JoinColumn(name="idcliente")
+	private Cliente cliente;
 	
 	public Sede() {
 		super();
 	}
 
-	public Sede(int idsede, String nombre, String direccion, String encargado, String telefono, String correo) {
+	public Sede(int idsede, String nombre, String direccion, String encargado, String telefono, String correo,
+			String estatus, Cliente cliente) {
 		super();
 		this.idsede = idsede;
 		this.nombre = nombre;
@@ -34,6 +41,8 @@ public class Sede {
 		this.encargado = encargado;
 		this.telefono = telefono;
 		this.correo = correo;
+		this.estatus = estatus;
+		this.cliente = cliente;
 	}
 
 	public int getIdsede() {
@@ -84,13 +93,27 @@ public class Sede {
 		this.correo = correo;
 	}
 
+	public String getEstatus() {
+		return estatus;
+	}
+
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
 	@Override
 	public String toString() {
 		return "Sede [idsede=" + idsede + ", nombre=" + nombre + ", direccion=" + direccion + ", encargado=" + encargado
-				+ ", telefono=" + telefono + ", correo=" + correo + "]";
+				+ ", telefono=" + telefono + ", correo=" + correo + ", estatus=" + estatus + ", cliente=" + cliente
+				+ "]";
 	}
-	
-	
-	
 	
 }
